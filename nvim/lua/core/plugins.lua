414e348913dd4cd6be22de2ca8c8978d624e0d37
JW <jombbey@gmail.com>
2023-09-14 22:38:48 +1000

    Add trouble plugin

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -95,8 +95,17 @@ local plugins = {
       lazy = false,
       priority = 1000,
       opts = {},
+  },
+  {
+      "folke/trouble.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      opts = {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+      }
+  },
   }
-}
 
 local opts = {}
 
