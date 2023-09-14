1c61a1df575231953e982a566f4f85336207c7f2
SharkPerson <38376803+SharkPerson@users.noreply.github.com>
2023-09-14 21:57:10 +1000

    Update keymaps.lua
    Added keymap for j k to be Esc.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+map('i', 'jk', '<Esc>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
